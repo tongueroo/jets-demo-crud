4580c088baa0a9a9a7a7b592211484172a3d2731
Tung Nguyen <tongueroo@gmail.com>
2018-09-03 19:47:55 +0000

    poc logic on how to render binary content

--- render_me.rb
+++ render_me.rb
@@ -1,6 +1,18 @@
 require "action_controller"
 require "rack/mime"
 
+require 'base64'
+require 'mimemagic'
+
+def binary?(filename)
+  begin
+    fm= FileMagic.new(FileMagic::MAGIC_MIME)
+    !(fm.file(filename)=~ /^text\//)
+  ensure
+    fm.close
+  end
+end
+
 def render_me
   renderer_options = {
     http_host: "localhost:8888",
@@ -9,12 +21,29 @@ def render_me
   renderer = ActionController::Base.renderer.new(renderer_options)
 
   catchall_path = "./public/test-logo.png"
-  # catchall_path = "./public/simple.html"
+  catchall_path = "./public/simple.html"
+
+  encoded_content = Base64.encode64(IO.read(catchall_path))
+
   content_type = Rack::Mime.mime_type(File.extname(catchall_path))
-  render_options = {
-    file: catchall_path,
-    content_type: content_type
-  }
+  puts "content_type #{content_type}"
+
+  binary = !MimeMagic.by_path(catchall_path).text?
+  puts "binary? #{binary}"
+
+  render_options = if binary
+                     {
+                       plain: "encoded_content...",
+                       content_type: content_type,
+                       layout: false,
+                     }
+                   else
+                     {
+                       file: catchall_path,
+                       content_type: content_type
+                     }
+                   end
+
   body = nil
   # begin
     body = renderer.render(render_options)
